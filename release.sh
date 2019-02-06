a6fb627c043d1876650b5ac4d2eaa778e547b0c8
Tom Hayden <thayden@gmail.com>
2019-02-06 14:43:03 -0600

    adding versioning

--- release.sh
+++ release.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 S3PATH="s3://hngr-icons/js/utm-passer.min.js"
-uglifyjs --compress --mangle -- utm-passer.js > utm-passer.min.js
+VERSION=`cat VERSION`
+echo "//version=$VERSION" > utm-passer.min.js
+uglifyjs --compress --mangle -- utm-passer.js >> utm-passer.min.js
 aws s3 cp utm-passer.min.js $S3PATH
+
+aws cloudfront create-invalidation --distribution-id "E28MNI4K0QJS6H" --paths "/js/utm-passer.min.js"
+
